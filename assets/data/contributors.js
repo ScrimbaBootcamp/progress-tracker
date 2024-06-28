b26717fa50eee18a2b5963af6f90f61c4c39592d
Geoffrey le Meur <geoffrey.lemeur@gmail.com>
2024-06-28 16:48:32 +0200

    added data

---
+++ assets/data/contributors.js
@@ -0,0 +1,25 @@
+[
+    {
+        name: "Micha",
+        linkedIn: "https://linkedin.com/in/michaella-rodriguez",
+        gitHub: "https://github.com/michaella23",
+    },
+    {
+        name: "Erik",
+        linkedIn: "https://www.linkedin.com/in/erik-ramirez-atl/",
+        gitHub: "https://github.com/eram23",
+        avatar: ""
+    },
+    {
+        name: "Fadumo Ahmed",
+        linkedIn: "#",
+        gitHub: "https://github.com/fame372",
+        avatar: "url('./assets/images/contributors/fadumo-avator-photo.jpeg')"
+    },
+    {
+        name: "Geofrey Le Meur",
+        linkedIn: "https://www.linkedin.com/in/geoffreylemeur/",
+        gitHub: "https://github.com/BuckyBuck135",
+        avatar: "url('./assets/images/contributors/geoffrey-avatar.png')"
+    },
+]
