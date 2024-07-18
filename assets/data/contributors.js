22d1d9df2f08f82502ae2e88dd6815f6b2cfba6f
R. Sam Craig <103201653+Samcraig77@users.noreply.github.com>
2024-07-18 02:25:25 -0700

    added alt text for avatar pictures, if available
    empty string added if no avatar

--- assets/data/contributors.js
+++ assets/data/contributors.js
@@ -3,57 +3,68 @@ const contributors = [
     name: 'Micha',
     linkedIn: 'https://linkedin.com/in/michaella-rodriguez',
     gitHub: 'https://github.com/michaella23',
+    avatar: '',
+    alt: ''
   },
   {
     name: 'Erik',
     linkedIn: 'https://www.linkedin.com/in/erik-ramirez-atl/',
     gitHub: 'https://github.com/eram23',
     avatar: '',
+    alt: ''
   },
   {
     name: 'Fadumo Ahmed',
     linkedIn: '#',
     gitHub: 'https://github.com/fame372',
     avatar: "url('./assets/images/contributors/fadumo-avator-photo.jpeg')",
+    alt: 'Fadumo wearing a long pink shirt, looking into the distance on a beach'
   },
   {
     name: 'Geofrey Le Meur',
     linkedIn: 'https://www.linkedin.com/in/geoffreylemeur/',
     gitHub: 'https://github.com/BuckyBuck135',
     avatar: "url('./assets/images/contributors/geoffrey-avatar.png')",
+    alt: 'Geofrey smiling, with a white outline, against a blue background'
   },
   {
     name: 'Paula Fenner',
     linkedIn: '',
     gitHub: 'https://github.com/paulaFenner',
     avatar: "url('./assets/images/contributors/paula-avatar.jpeg')",
+    alt: ''
   }, 
   {
     name: 'Sam Craig',
     linkedIn: 'https://www.linkedin.com/in/sam-craig-059834252/',
     gitHub: 'https://github.com/Samcraig77',
-    avatar: "url('.assets/images/contributors/Sam-avatar.jpg')"
+    avatar: "url('.assets/images/contributors/Sam-avatar.jpg')",
+    alt: ''
 },
 ];
 
 const contributorSection = document.getElementById("contributors-section")
 
-function renderContributors() {
-for (i = 0; i < 0; i++) {
-
+function getContributors(people) {
+for (let contributor of contributors){
+  console.log(contributor)
 }
 }
 
-function createContributorSlide(array) {
+console.log(getContributors(contributors))
 
-array = Object.values 
+// function createContributorSlide(array) {
 
-contributorSection.innerHTML += `
-<section class="contributor-slide">
-<h2>${contributors.name}</h2>
-<img src="${contributors.avatar}" alt="">
-<i class="linkedIn-icon" href="${contributors.linkedIn}">LinkedIn</a>
-<i class="gitHub-icon" href="${contributors.gitHub}">Github</a>
+// // array = Object.values 
+
+
+
+// // contributorSection.innerHTML += `
+// // <section class="contributor-slide">
+// // <h2>${contributors.name}</h2>
+// // <img src="${contributors.avatar}" alt="">
+// // <i class="linkedIn-icon" href="${contributors.linkedIn}">LinkedIn</a>
+// // <i class="gitHub-icon" href="${contributors.gitHub}">Github</a>
  
-</section>`
-}
+// // </section>`
+// // }
