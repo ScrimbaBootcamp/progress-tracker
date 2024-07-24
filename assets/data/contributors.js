737c740cbcdb46b13908317fd0a501e43d140ea2
R. Sam Craig <103201653+Samcraig77@users.noreply.github.com>
2024-07-24 15:16:57 -0700

    Changed user avatar values to use file paths
    The previous url('') format was impossible to use within an <img>.
    Added alt text to describe contributors avatars.

--- assets/data/contributors.js
+++ assets/data/contributors.js
@@ -3,7 +3,7 @@ export const contributorsArray = [
     name: 'Sam Craig',
     linkedIn: 'https://www.linkedin.com/in/sam-craig-059834252/',
     gitHub: 'https://github.com/Samcraig77',
-    avatar: "url('./assets/images/contributors/Sam-avatar.jpg')",
+    avatar: '/assets/images/contributors/Sam-avatar.jpg',
     alt: 'Sam looking at the camera, with an out of focus tree in the background.'
   },
   {
@@ -24,21 +24,21 @@ export const contributorsArray = [
     name: 'Fadumo Ahmed',
     linkedIn: '',
     gitHub: 'https://github.com/fame372',
-    avatar: "url('./assets/images/contributors/fadumo-avator-photo.jpeg')",
+    avatar: '/assets/images/contributors/fadumo-avator-photo.jpeg',
     alt: 'Fadumo wearing a long pink shirt, looking into the distance on a beach.'
   },
   {
     name: 'Geofrey Le Meur',
     linkedIn: 'https://www.linkedin.com/in/geoffreylemeur/',
     gitHub: 'https://github.com/BuckyBuck135',
-    avatar: "url('./assets/images/contributors/geoffrey-avatar.png')",
+    avatar: '/assets/images/contributors/geoffrey-avatar.png',
     alt: 'Geofrey smiling, with a white outline, against a blue background.'
   },
   {
     name: 'Paula Fenner',
     linkedIn: '',
     gitHub: 'https://github.com/paulaFenner',
-    avatar: "url('./assets/images/contributors/paula-avatar.jpeg')",
+    avatar: '/assets/images/contributors/paula-avatar.jpeg',
     alt: 'Paula happily smiling at the camera with her eyes closed.'
-  }, 
+  },
 ];
